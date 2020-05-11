c7b822916ba9832593c03c93b1a703eed46d2091
Gema Saputera <gema.saputera@gmail.com>
2020-05-11 16:30:00 +0700

    Enable Regitrations

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'DashboardController@index')->name('dashboard');
 
-Auth::routes(['register'=>false]);
+Auth::routes();
 
 Route::get('products/{id}/gallery', 'ProductController@gallery')->name('products.gallery');
 Route::get('transactions/{id}/set-status', 'TransactionController@setStatus')->name('transactions.status');
