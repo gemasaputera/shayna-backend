3370042be5cee763c05cd5d3017e94aa09ca4692
Gema Saputera <gema.saputera@gmail.com>
2020-05-11 16:32:03 +0700

    disabled regitration

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'DashboardController@index')->name('dashboard');
 
-Auth::routes();
+Auth::routes(['register'=>false]);
 
 Route::get('products/{id}/gallery', 'ProductController@gallery')->name('products.gallery');
 Route::get('transactions/{id}/set-status', 'TransactionController@setStatus')->name('transactions.status');
